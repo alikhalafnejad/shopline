7224024d41c1301f19c38c5090f78cfc707d2d79
alikhalafnejad <a.kh.f1994@gmail.com>
2025-02-11 17:34:35 +0330

    fix jwt file

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"github.com/golang-jwt/jwt/v5"
+	"shopline/pkg/constants"
 	"time"
 )
 
@@ -15,5 +16,29 @@ type Claims struct {
 
 // GenerateToken generates a JWT token for the given user ID.
 func GenerateToken(userID uint) (string, error) {
-	expirationTime := time.Now().Add(24 * time.Hour)
+	expirationTime := time.Now().Add(constants.JWTDuration)
+	claims := &Claims{
+		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(constants.JWTSecretKey))
+}
+
+// ValidateToken validates a JWT token and returns the claims if valid
+func ValidateToken(tokenString string) (*Claims, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(constants.JWTSecretKey), nil
+	})
+
+	if err != nil || !token.Valid {
+		return nil, err
+	}
+
+	return claims, nil
 }
