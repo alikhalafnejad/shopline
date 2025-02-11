6dba4ab25472a38afcae47022d5899e6599c2d90
alikhalafnejad <a.kh.f1994@gmail.com>
2025-02-11 17:50:57 +0330

    adding redis client file

--- pkg/redisdb/redis.go
+++ pkg/redisdb/redis.go
@@ -3,10 +3,29 @@ package redisdb
 import (
 	"context"
 	"github.com/redis/go-redis/v9"
+	"log"
+	"shopline/config"
 )
 
 var ctx = context.Background()
 
 type RedisClient struct {
 	Client *redis.Client
+}
+
+func NewRedisClient() *RedisClient {
+	settings := config.LoadSettings()
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     settings.RedisAddr,
+		Password: settings.RedisPassword,
+		DB:       settings.RedisDB,
+	})
+
+	_, err := client.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("Failed to connect to Redis: %v", err)
+	}
+
+	return &RedisClient{Client: client}
 }
