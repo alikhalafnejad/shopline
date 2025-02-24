b5f5603225abb6230756033c820f97cb5b07a82c
alikhalafnejad <a.kh.f1994@gmail.com>
2025-02-24 17:12:46 +0330

    caching

--- internal/repositories/base_repo.go
+++ internal/repositories/base_repo.go
@@ -1,13 +1,79 @@
 package repositories
 
-import "gorm.io/gorm"
+import (
+	"context"
+	"gorm.io/gorm"
+	"reflect"
+	"shopline/pkg/cache"
+	"time"
+)
 
 type BaseRepository struct {
-	DB *gorm.DB
+	DB    *gorm.DB
+	Cache *cache.CachedExecutor
 }
 
-func NewBaseRepository(db *gorm.DB) *BaseRepository {
-	return &BaseRepository{DB: db}
+func NewBaseRepository(db *gorm.DB, cacheClient cache.Client) *BaseRepository {
+	return &BaseRepository{
+		DB:    db,
+		Cache: cache.NewCachedExecutor(cacheClient),
+	}
+}
+
+type CachedFindOption func() []cache.CacheableFunc
+
+func WithCache(key string, ttl time.Duration) CachedFindOption {
+	return func() []cache.CacheableFunc {
+		return []cache.CacheableFunc{
+			cache.WithKey(key),
+			cache.WithTTL(ttl),
+		}
+	}
+}
+
+func (r *BaseRepository) CachedFindOne(ctx context.Context, out interface{}, query interface{}, opts ...CachedFindOption) error {
+	var cacheOpts []cache.CacheableFunc
+	for _, opt := range opts {
+		cacheOpts = append(cacheOpts, opt()...)
+	}
+
+	fn := func() (interface{}, error) {
+		result := r.DB.Where(query).First(out)
+		return result, result.Error
+	}
+
+	val, err := r.Cache.Execute(ctx, fn, cacheOpts...)
+	if err != nil {
+		return err
+	}
+
+	reflect.ValueOf(out).Elem().Set(reflect.ValueOf(val).Elem())
+	return nil
+}
+
+func (r *BaseRepository) InvalidateCache(ctx context.Context, keys ...string) error {
+	for _, key := range keys {
+		if err := r.Cache.Delete(ctx, key); err != nil {
+			return err
+		}
+	}
+}
+
+func (r *BaseRepository) CachedFind(ctx context.Context, query interface{}, opts ...CachedFindOption) (interface{}, error) {
+	var cacheOpts []cache.CacheableFunc
+	for _, opt := range opts {
+		cacheOpts = append(cacheOpts, opt()...)
+	}
+	fn := func() (interface{}, error) {
+		result := r.DB.Find(query)
+		return result, result.Error
+	}
+	val, err := r.Cache.Execute(ctx, fn, cacheOpts...)
+	if err != nil {
+		return nil, err
+	}
+	reflect.ValueOf(query).Elem().Set(reflect.ValueOf(val).Elem())
+	return val, nil
 }
 
 // Create inserts a new record into the database.
--- pkg/cache/cacheable.go
+++ pkg/cache/cacheable.go
@@ -2,6 +2,10 @@ package cache
 
 import (
 	"context"
+	"fmt"
+	"go.uber.org/zap"
+	"reflect"
+	"shopline/pkg/logger"
 	"time"
 )
 
@@ -43,11 +47,69 @@ func (c *CachedExecutor) Execute(ctx context.Context, fn interface{}, opts ...Ca
 		return c.callFunction(fn)
 	}
 
-	cacheKey := c.
+	cacheKey := c.generateCacheKey(fn, config.KeyTemplate)
+	var result interface{}
+
+	// Try to get from cache
+	err := c.client.Get(cacheKey, &result)
+	if err == nil {
+		return result, nil
+	}
+
+	// Cache miss - execute function
+	val, err := c.callFunction(fn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set to cache
+	if err := c.client.Set(cacheKey, val, config.TTL); err != nil {
+		logger.Logger.Error("Failed to set cache", zap.String("key", cacheKey), zap.Error(err))
+	}
+
+	return val, nil
 }
 
-func (c *CachedExecutor) generateCacheKey(fn interface{}, template string) string {}
+func (c *CachedExecutor) generateCacheKey(fn interface{}, template string) string {
+	fv := reflect.ValueOf(fn)
+	args := make([]interface{}, fv.Type().NumIn())
+
+	for i := 0; i < fv.Type().NumIn(); i++ {
+		args[i] = fv.Index(i).Interface()
+	}
+
+	return fmt.Sprintf(template, args...)
+}
 
 func (c *CachedExecutor) callFunction(fn interface{}) (interface{}, error) {
+	fv := reflect.ValueOf(fn)
+	if fv.Kind() != reflect.Func {
+		return nil, fmt.Errorf("non-function passed to Execute")
+	}
 
+	args := make([]reflect.Value, fv.Type().NumIn())
+	for i := range args {
+		if i == 0 {
+			// Assume first argument is context.Context
+			args[i] = reflect.ValueOf(context.Background())
+		} else {
+			// Initialize zero values for other arguments
+			args[i] = reflect.Zero(fv.Type().In(i))
+		}
+	}
+
+	results := fv.Call(args)
+	if len(results) == 0 {
+		return nil, nil
+	}
+
+	// Handle error as last return value
+	var err error
+	if len(results) > 1 {
+		if e, ok := results[len(results)-1].Interface().(error); ok {
+			err = e
+		}
+	}
+
+	return results[0].Interface(), err
 }
